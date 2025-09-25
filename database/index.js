9664bffe6900ba8ad7a94891641a1d57ba87be0a
Vitalina Syzova <sizovavitalina@gmail.com>
2025-09-25 18:17:45 +0200

    fix for render

--- database/index.js
+++ database/index.js
@@ -1,37 +1,59 @@
+// const { Pool } = require("pg")
+// require("dotenv").config()
+// /* ***************
+//  * Connection Pool
+//  * SSL Object needed for local testing of app
+//  * But will cause problems in production environment
+//  * If - else will make determination which to use
+//  * *************** */
+// let pool
+// if (process.env.NODE_ENV == "development") {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   })
+
+// // Added for troubleshooting queries
+// // during development
+//   module.exports = {
+//     async query(text, params) {
+//       try {
+//         const res = await pool.query(text, params)
+//         // console.log("executed query", { text })
+//         return res
+//       } catch (error) {
+//         console.error("error in query", { text })
+//         throw error
+//       }
+//     },
+//   }
+// } else {
+//   pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//   })
+//   module.exports = pool
+// }
+
 const { Pool } = require("pg")
 require("dotenv").config()
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
-let pool
-if (process.env.NODE_ENV == "development") {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  })
 
-// Added for troubleshooting queries
-// during development
-  module.exports = {
-    async query(text, params) {
-      try {
-        const res = await pool.query(text, params)
-        // console.log("executed query", { text })
-        return res
-      } catch (error) {
-        console.error("error in query", { text })
-        throw error
-      }
-    },
-  }
-} else {
-  pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  })
-  module.exports = pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
+module.exports = {
+  async query(text, params) {
+    try {
+      const res = await pool.query(text, params)
+      return res
+    } catch (error) {
+      console.error("❌ Error in query:", { text, error: error.message })
+      throw error
+    }
+  },
 }
